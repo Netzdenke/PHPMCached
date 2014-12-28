b3a59f64bfe30fd9b4206e658c23930d8d5507cd
Chris Herden <contact@chris-herden.de>
2014-12-28 17:20:58 +0100

    add test bootstrap file

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the PHPMCached package.
+ *
+ * (c) Hahn und Herden Netzdenke GbR <info@netzdenke.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once dirname(__FILE__) . '/../src/PHPMCached/PHPMCached.php';
